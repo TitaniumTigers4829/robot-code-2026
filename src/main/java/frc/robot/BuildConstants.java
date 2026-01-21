b18fd5b814835647cf69603e2cb58137a6278989
aridavidson001 <aridavidson6@gmail.com>
2026-01-21 17:15:28 -0500

    huh

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "mako-robot-code-2025-6";
+  public static final String MAVEN_NAME = "robot-code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 288;
-  public static final String GIT_SHA = "d6468612c1a125301390f1cd886e73389062d9ae";
-  public static final String GIT_DATE = "2025-11-14 18:05:23 EST";
-  public static final String GIT_BRANCH = "use-tigerhelpers";
-  public static final String BUILD_DATE = "2026-01-10 13:06:18 EST";
-  public static final long BUILD_UNIX_TIME = 1768068378333L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 31;
+  public static final String GIT_SHA = "ba066e60f1cf8044482bf7744220608044618df3";
+  public static final String GIT_DATE = "2026-01-21 17:13:34 EST";
+  public static final String GIT_BRANCH = "super-awesome-shooter";
+  public static final String BUILD_DATE = "2026-01-21 17:14:01 EST";
+  public static final long BUILD_UNIX_TIME = 1769033641582L;
+  public static final int DIRTY = 0;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
